15c8d52e6ee9d4c1240a31dbac473998f04ea613
Renan Araújo <renan.alvs@gmail.com>
2024-07-08 17:40:33 -0300

    Fixed loading position to the screen center

--- components/misc/Loading.jsx
+++ components/misc/Loading.jsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { GiRat } from "react-icons/gi"
+import { useRef } from "react"
 
 export default function Loading({ action }) {
+    const getWindowTop = useRef(window.visualViewport.pageTop)
+
     return (
-        <div className={`absolute flex flex-col items-center justify-center left-0 top-0 w-full min-h-screen text-3xl bg-[rgba(0,0,0,0.95)] backdrop-blur-sm`}>
+        <div style={{ top: `${getWindowTop.current}px` }} className={`absolute flex flex-col items-center justify-center left-0 w-full min-h-screen text-3xl bg-[rgba(0,0,0,0.95)] backdrop-blur-sm`}>
             <GiRat  size={128} />
             <span>
                 { action }
